b65286664426d003eca0f17e614f9f3cb618542a
lucas reis <reis83212@gmail.com>
2024-05-02 11:15:08 -0300

    anotações

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,11 +1,19 @@
+// Define uma função assíncrona chamada listaVideos
 async function listaVideos() {
+    // Realiza uma solicitação para o servidor local para obter a lista de vídeos
     const conexao = await fetch("http://localhost:3000/videos"); 
+
+    // Converte a resposta da solicitação para JSON
     const conexaoConvertida = await conexao.json();
+
+    // Exibe os dados recebidos da API em formato de tabela no console do navegador
     console.table(conexaoConvertida);
 
-    return conexaoConvertida
+    // Retorna os dados recebidos da API
+    return conexaoConvertida;
 }
 
+// Exporta um objeto chamado conectaApi, que contém a função listaVideos
 export const conectaApi = {
     listaVideos
 }
